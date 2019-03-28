49fe72f1ce80d1f230c2b7c92827eb5610b9945a
Reinaldo Ramos <masterrey@gmail.com>
2019-03-27 22:23:21 -0300

    turret como ia e radar.

--- Assets/Codes/Ship3DControl.cs
+++ Assets/Codes/Ship3DControl.cs
@@ -15,16 +15,18 @@ public class Ship3DControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        inputaxis = new Vector3(Input.GetAxis("Vertical"), 0,-Input.GetAxis("Horizontal"));
+       
     }
     void FixedUpdate()
     {
+        inputaxis = new Vector3(Input.GetAxis("Vertical") * 10000, 0, -Input.GetAxis("Horizontal") * 40000);
+
         float trail= Vector3.Angle(transform.forward, rdb.velocity.normalized)*0.01f;
         tr.time = trail;
         tl.time = trail;
 
         //adiciona o torque relativo para movimento avionico
-        rdb.AddRelativeTorque(inputaxis * 10000);
+        rdb.AddRelativeTorque(inputaxis);
         //calcula o roll da aeronave pelo angulo dos eixos
         float roll = Vector3.Angle(transform.right, Vector3.up)-90;
         //adiciona torque global para curva 
---
+++ Assets/Codes/Turret.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    public GameObject support, pipe;
+    GameObject target;
+    public ParticleSystem fire;
+    bool enableshoot = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target= GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enableshoot)
+        {
+            float dist = Vector3.Distance(target.transform.position, transform.position);
+            pipe.transform.LookAt(target.transform.position + target.transform.forward * dist * 0.5f);
+
+            support.transform.rotation = Quaternion.Euler(
+                -90, pipe.transform.rotation.eulerAngles.y, 0);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        enableshoot = true;
+        fire.Play();
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        fire.Stop();
+        enableshoot = false;
+    }
+}
