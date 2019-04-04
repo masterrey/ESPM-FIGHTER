b5b8d88088c032ecfa7d941bbba5341650e72778
Reinaldo Ramos <masterrey@gmail.com>
2019-04-03 22:28:59 -0300

    missile

---
+++ Assets/Codes/Missile.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+{
+    GameObject target;
+    Rigidbody rdb;
+    public ParticleSystem smoke;
+    public AudioSource aud;
+    float count =0.5f;
+    bool active = false;
+    public GameObject explosionPrefab;
+    void Start()
+    {
+        rdb = GetComponent<Rigidbody>();
+    }
+    public void SetTarget(GameObject tgt)
+    {
+        active = true;
+        aud.Play();
+        smoke.Play();
+        rdb.isKinematic = false;
+        target = tgt;
+        Destroy(gameObject, 5);
+        transform.parent = null;
+    }
+    
+    public void SetTarget()
+    {
+        active = true;
+        aud.Play();
+        smoke.Play();
+        rdb.isKinematic = false;
+        Destroy(gameObject, 5);
+        transform.parent = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (active)
+        {
+            count -= Time.deltaTime;
+        }
+        if (target)
+        {
+            if(count<0)
+            transform.LookAt(target.transform);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (count < 0)
+        {
+            Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+            RaycastHit[] all = Physics.SphereCastAll(transform.position, 100, Vector3.down);
+            foreach(RaycastHit hit in all){
+                Destroy(hit.collider.gameObject);
+            }
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Codes/RadarMissile.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadarMissile : MonoBehaviour
+{
+    public Missile[] missile;
+    int index=0;
+    GameObject lasttarget;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            if (lasttarget)
+            {
+                missile[index].SetTarget(lasttarget);
+            }
+            else
+            {
+                missile[index].SetTarget();
+            }
+            index++;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("enemy"))
+        {
+            lasttarget = other.gameObject;
+            print(other.gameObject.name);
+        }
+    }
+}
